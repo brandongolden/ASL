a6a2cbf2181424c2f55d1cec53a6b38a46132afa
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-09-22 13:19:19 -0400

    Delete chart.js files
    Using chart.js cdn

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<title>My Application</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/0.2.0/Chart.min.js" type="text/javascript"></script>
+
 
 </head>
 <body>
--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -3,6 +3,34 @@
 @section('content')
 <h2>Homepage for user {{ \Auth::user()->name }}</h2>
 <hr />
+<canvas id="skills" width="300" height="300"></canvas>
+
+<script>
+var pieData = [
+   {
+      value: 25,
+      label: 'Java',
+      color: '#811BD6'
+   },
+   {
+      value: 10,
+      label: 'Scala',
+      color: '#9CBABA'
+   },
+   {
+      value: 30,
+      label: 'PHP',
+      color: '#D18177'
+   },
+   {
+      value : 35,
+      label: 'HTML',
+      color: '#6AE128'
+   }
+];
+var context = document.getElementById('skills').getContext('2d');
+var skillsChart = new Chart(context).Pie(pieData);
+</script>
 
 <?php
 	$clientid = Auth::user()->id;
